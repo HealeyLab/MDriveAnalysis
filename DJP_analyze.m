e14d9cbcfcf01aa196cf3ae1cf028e1ed970b482
Dan Pollak <danpollak23@gmail.com>
2018-04-11 20:50:57 -0400

    class stims

--- DJP_analyze.m
+++ DJP_analyze.m
@@ -9,6 +9,9 @@ fid = fopen(fullfile(stim_text_file.folder, stim_text_file.name));
 stim_order = textscan(fid, '%s', 'Delimiter', '\n');
 fclose(fid);
 
+stim_order = stim_order{1};
+stim_classes = unique(stim_order);
+
 for j=1:length(files)
 % For each channel
     curr_file = fullfile(files(j).folder, files(j).name)
@@ -17,63 +20,71 @@ for j=1:length(files)
     num_classes = max(cluster_class(:,1))
     load(fullfile(files(j).folder, 'adc_data.mat')); % adc_dat is the variable
      
-    for i = 1:num_classes 
-        %% For each class of cells   
-        % we ignore class 0, the garbage spikes
-        % cluster_class is in ms!
-        I = find(cluster_class(:,1) == i); % all indices of cluster class i
-        sp_t = cluster_class(I,2);
-        diff_data = diff([0,adc_dat,0]);
-        jump_start  = find(diff_data > 1);
+    %% For each stimulus class
+    for m = 1:length(stim_order)
         
-        % raster and histo parameters
-        window = 2000; % 1 s or 1000 ms
-        left = window * .1;
-        right = window * .9;
-               
-        %% For each stimulus class
-        %% For each stim time, put in raster
-        spike_times=cell(length(jump_start),1);
-        for k = 1:length(jump_start)
-            % Putting data in raster, window is in ms
-            % Note: adc_sr is a variable saved with adc_data
-            curr = 1 / adc_sr * 1000 * jump_start(k); % converting from samples to ms
-             % next two lines: 100 ms before, 3900 ms after threshold cross. more
-                % like 3500 ms after stim onset.
-            spike_times{k} = 1 / 1000 * (intersect(...
-                sp_t(sp_t > (curr - left))',...
-                sp_t(sp_t < (curr + right))') - curr); % centers, converts ms to seconds
+        % For each class of cells
+        for i = 1:num_classes
+            % we ignore class 0, the garbage spikes
+            % cluster_class is in ms!
+            I = find(cluster_class(:,1) == i); % all indices of cluster class i
+            sp_t = cluster_class(I,2);
+            diff_data = diff(adc_dat);
+            jump_start  = find(diff_data > 1);
             
-
+            % Only for the stimulus class we want:
+            stim_indices = ismember(stim_order, stim_classes{m});
+            jump_start = jump_start(stim_indices);
+            
+            % raster and histo parameters
+            window = 2000; % 1 s or 1000 ms
+            left = window * .1;
+            right = window * .9;
+            
+            
+            %% For each stim time, put in raster
+            spike_times=cell(length(jump_start),1);
+            for k = 1:length(jump_start)
+                % Putting data in raster, window is in ms
+                % Note: adc_sr is a variable saved with adc_data
+                curr = 1 / adc_sr * 1000 * jump_start(k); % converting from samples to ms
+                % next two lines: 100 ms before, 3900 ms after threshold cross. more
+                % like 3500 ms after stim onset.
+                spike_times{k} = 1 / 1000 * (intersect(...
+                    sp_t(sp_t > (curr - left))',...
+                    sp_t(sp_t < (curr + right))') - curr); % centers, converts ms to seconds
+                
+                
+            end
+            % TODO: I have made subplot(~,1,~) subplot(~,2,~), allowing a space
+            % next to the graphs in which I intend to display the waveform, as
+            % well as the width and summetry of the waveform, displayed in a
+            % text box, I think. This info will be extractable from the fig,
+            % I'm pretty sure. We want to make this so that the waveofrm is edited in the
+            % subplot, so you see "is this good?" and the findchangepts next to
+            % the histogram. If there are multiple spike classes, we plot them together to see
+            % how their latencies compare
+            handle=figure; % this will be the easel on which we paint our poorly spelled masterpiece.
+            height = 2;
+            width = num_classes;
+            
+            raster_axes=subplot(height, width, i);
+            if i == 1
+                title_axes=raster_axes;
+            end
+            
+            [xpoints, ~]=plotSpikeRaster(spike_times, 'PlotTYpe','vertline');
+            
+            histo_axes = subplot(height, width, i+num_classes);
+            bin_ms = 20; % bin size in ms
+            histogram(histo_axes, xpoints, (-left/1000):bin_ms/1000:(right/1000));
+            ax=gca;
+            set(ax, 'XLim', [-left/1000 right/1000]);
+            
+            %% Waveform analysis
+            % put this is another script
+            %         [width,ratio]=DJP_waveform(spikes,I);
         end
-         % TODO: I have made subplot(~,1,~) subplot(~,2,~), allowing a space
-        % next to the graphs in which I intend to display the waveform, as
-        % well as the width and summetry of the waveform, displayed in a
-        % text box, I think. This info will be extractable from the fig,
-        % I'm pretty sure. We want to make this so that the waveofrm is edited in the
-        % subplot, so you see "is this good?" and the findchangepts next to
-        % the histogram. If there are multiple spike classes, we plot them together to see
-        % how their latencies compare
-        handle=figure; % this will be the easel on which we paint our poorly spelled masterpiece.
-        height = 2;
-        width = num_classes;
-        
-        raster_axes=subplot(height, width, i);
-        if i == 1
-            title_axes=raster_axes;
-        end
-        
-        [xpoints, ~]=plotSpikeRaster(spike_times, 'PlotTYpe','vertline');
-        
-        histo_axes = subplot(height, width, i+num_classes);
-        bin_ms = 20; % bin size in ms
-        histogram(histo_axes, xpoints, (-left/1000):bin_ms/1000:(right/1000));
-        ax=gca;
-        set(ax, 'XLim', [-left/1000 right/1000]);
-        
-        %% Waveform analysis
-        % put this is another script
-%         [width,ratio]=DJP_waveform(spikes,I);
     end
     %% format & save graph
     fig_file_name=strcat(...
